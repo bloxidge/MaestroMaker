7d42ab45a4e1e3eb07ddf2c94736369fa5f956b3
Peter Bloxidge <peter.bloxidge@virgin.net>
2017-04-18 13:33:27 +0100

    Initial project commit

---
+++ MaestroMaker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MaestroMaker WatchKit Extension
+//
+//  Created by Peter Bloxidge on 17/04/2017.
+//  Copyright © 2017 Peter Bloxidge. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
