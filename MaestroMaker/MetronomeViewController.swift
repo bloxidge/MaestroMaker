312aa50962689628bcb64ce1aaadfb87d42f1eaa
Peter Bloxidge <peter.bloxidge@virgin.net>
2017-05-24 23:38:44 +0100

    Added UI controls to metronome practice

--- MaestroMaker/MetronomeViewController.swift
+++ MaestroMaker/MetronomeViewController.swift
@@ -10,22 +10,50 @@ import UIKit
 
 class MetronomeViewController: UIViewController {
     
+    @IBOutlet var controlsView: UIView! {
+        didSet {
+            controlsView.layer.cornerRadius = 10
+            controlsView.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet var timeSigControl: UISegmentedControl!
+    @IBOutlet var tempoLabel: UILabel!
+    @IBOutlet var tempoSlider: UISlider!
+    @IBOutlet var startStopButton: UIButton!
+    
+    var isRunning = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.barStyle = .blackOpaque
         navigationController?.navigationBar.isTranslucent = false
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     override func willMove(toParentViewController parent: UIViewController?) {
         navigationController?.navigationBar.barStyle = .default
         navigationController?.navigationBar.isTranslucent = true
     }
-
+    
+    @IBAction func tempoChanged(_ sender: UISlider) {
+        sender.value = roundf(sender.value)
+        tempoLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func startStopButtonPressed(_ sender: UIButton) {
+        switch isRunning {
+        case true:
+            isRunning = false
+            sender.setTitle("START", for: .normal)
+            timeSigControl.isEnabled = true
+            tempoSlider.isEnabled = true
+        case false:
+            isRunning = true
+            sender.setTitle("STOP", for: .normal)
+            timeSigControl.isEnabled = false
+            tempoSlider.isEnabled = false
+        }
+    }
+    
 }
